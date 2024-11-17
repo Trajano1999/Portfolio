fbaf75408272a136b8d0557435952cc90cbe977a
Trajano1999 <juanmanuel.mateosperez@gmail.com>
2024-11-17 16:18:11 +0100

    adding my first component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+// Components
+import { Button } from './components/Button'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <button>Hola mundo</button>
+    <Button />
   </StrictMode>
 )
